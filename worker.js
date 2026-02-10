e5ee2aa77fd772df8db2b839ac9fabfe615a7022
Guilherme Montenegro <info@guimontenegro.dev>
2026-02-10 03:48:00 -0300

    Fix

--- worker.js
+++ worker.js
@@ -47,6 +47,8 @@ export default {
 
     if (!fbEventName && !gaEventName && !gadsConversionLabel) return new Response('Event/conversion is missing', { status: 400, headers: corsHeaders })
 
+    if (!eventUrl) return new Response('Event URL is missing', { status: 400, headers: corsHeaders })
+
     const headers = Object.fromEntries(request.headers.entries())
 
     const clientIp = headers['cf-connecting-ip'] ||
@@ -54,8 +56,6 @@ export default {
       null
 
     const userAgent = headers['user-agent'] || null
-
-    if (!referrer) return new Response('Referer header is missing', { status: 400, headers: corsHeaders })
 
     ctx.waitUntil(handleEvent(fbEventName, gaEventName, gadsConversionLabel, eventId, eventUrl, userId, cookieFbp, cookieFbc, cookieGclid, clientIp, userAgent, env))
 
